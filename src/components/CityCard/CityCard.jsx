e97e92ddd4c7f67a53188256d0e4cd4e22ca891b
Victor Felix Coman <felixvictor_coman@yahoo.com>
2024-06-10 13:12:00 +0300

    adaugare CityCard

---
+++ src/components/CityCard/CityCard.jsx
@@ -0,0 +1,20 @@
+import { Card } from "react-bootstrap";
+import { ButtonLandmark } from "../DestinationCard/DestinationCard.style";
+// const compactDataDestination = dataDestination ? dataDestination[0] : null;
+// console.log("compactDataDestination", compactDataDestination);
+
+function CityCard({ name, image, description, popularity }) {
+  return (
+    <Card style={{ width: "18rem" }}>
+      <Card.Img variant="top" src={image} alt="Image of landmark" />
+      <Card.Body>
+        <Card.Title>{name}</Card.Title>
+        <Card.Text>{description}</Card.Text>
+        <ButtonLandmark loc="ButtonLandmark" to={`/intinerary`}>
+          Save landmark to my intinerary!
+        </ButtonLandmark>
+      </Card.Body>
+    </Card>
+  );
+}
+export default CityCard;
