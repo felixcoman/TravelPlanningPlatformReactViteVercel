25ed0d02b12fbca40aefa0ad499e24ccc1458eea
Victor Felix Coman <felixvictor_coman@yahoo.com>
2024-06-10 20:59:00 +0300

    afisare date global state si aducere date de pe server - functional; mecanismul se foloseste de componenta noua CityCard pentru afisare

--- src/components/Intinerary/Intinerary.jsx
+++ src/components/Intinerary/Intinerary.jsx
@@ -1,6 +1,5 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import { IntineraryContext } from "../../global/context";
-import useFetchData from "../../hooks/useFetchData";
 import CityCard from "../CityCard/CityCard";
 import { SectionIntineraryData } from "./Intinerary.style";
 
@@ -11,31 +10,12 @@ function Intinerary() {
 
   console.log("intineraryValueArray", intineraryValueArray);
 
-  const [clicked, setClicked] = useState(true);
-
-  let url = null;
-
-  const createFetchUrl = (arr) => {
-    arr.map((locPair) => {
-      console.log("locPair", locPair);
-      if (locPair.country && locPair.city) {
-        url = `http://localhost:3001/${locPair.country}?city=${locPair.city}`;
-        console.log("url", url);
-      } else null;
-    });
-  };
-
-  createFetchUrl(intineraryValueArray);
-
-  const { data, error, loading } = useFetchData(url, clicked, setClicked);
-  console.log("data", "error", "loading", data, error, loading);
-
   return (
     <>
       <SectionIntineraryData loc="SectionIntineraryData">
-        {data &&
-          data?.map((destination, index) => (
-            <CityCard key={index} {...destination} />
+        {stateGlobalIntinerary &&
+          intineraryValueArray?.map((element, index) => (
+            <CityCard key={index} {...element} />
           ))}
       </SectionIntineraryData>
     </>
